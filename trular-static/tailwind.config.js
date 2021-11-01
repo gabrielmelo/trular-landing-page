562939df16191e8947f47d8a19e80cce27ec377b
Gabriel Melo <gblsml0@gmail.com>
2021-11-01 16:47:19 -0300

    Add files config

---
+++ trular-static/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1440px',
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
