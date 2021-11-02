04634d3311a9d2736be838f63aa7c8d3622f2bf7
Gabriel Melo <gblsml0@gmail.com>
2021-11-02 15:39:49 -0300

    Configuration layout and components

--- trular-static/tailwind.config.js
+++ trular-static/tailwind.config.js
@@ -3,13 +3,24 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1440px',
-    }
+      sm: '640px',
+      md: '768px',
+      lg: '1440px',
+    },
+    extend: {
+      colors: {
+        blue: {
+          50: '#E5F3FF',
+        },
+        primary: '#0072D3',
+      },
+    },
   },
   variants: {
     extend: {},
   },
+  corePlugins: {
+    container: false,
+  },
   plugins: [],
-}
+};
