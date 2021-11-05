43018164e799488cbcb52b1ae9f6469ffbcf68e5
Gabriel Melo <gblsml0@gmail.com>
2021-11-04 23:21:39 -0300

    fix(layout responsive): fix bugs layout responsive, images & remove parallax

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,17 @@
-// import LocomotiveScroll from 'locomotive-scroll';
+// // import LocomotiveScroll from 'locomotive-scroll';
 
-// const scroll = new LocomotiveScroll();
+// // const scroll = new LocomotiveScroll();
 
-// import LocomotiveScroll from 'locomotive-scroll';
+// // import LocomotiveScroll from 'locomotive-scroll';
 
-const scroller = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
-  smooth: true,
-});
+// const scroller = new LocomotiveScroll({
+//   el: document.querySelector('.l-main-content'),
+//   smooth: true,
+//   // // lerp: 0.4,
+//   multiplier: 2,
+//   touchMultiplier: 4,
+//   scrollFromAnywhere: true,
+//   // // repeat: true,
+//   // offset: ['30%', 0],
+//   // reloadOnContextChange: true,
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: ['/index.html', './src/styles/**/*.scss'],
+  purge: ['/index.html', './src/styles/**/*.css'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
       sm: '640px',
       md: '768px',
-      lg: '1440px',
+      lg: '1200px',
       xl: '1550px',
     },
     extend: {
@@ -14,8 +14,11 @@ module.exports = {
           50: '#E5F3FF',
         },
         primary: '#0072D3',
-      },
+      }
     },
+    fontFamily: {
+      'sans': ['Inter', 'sans serif'],
+    }
   },
   variants: {
     extend: {},
