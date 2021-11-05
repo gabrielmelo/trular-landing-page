632a610014a75005ae820f2ec6046a932d2f91a8
Gabriel Melo <gblsml0@gmail.com>
2021-11-05 12:02:12 -0300

    feat(parallax): create parallax with rellax.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,4 @@
-// // import LocomotiveScroll from 'locomotive-scroll';
 
-// // const scroll = new LocomotiveScroll();
 
-// // import LocomotiveScroll from 'locomotive-scroll';
-
-// const scroller = new LocomotiveScroll({
-//   el: document.querySelector('.l-main-content'),
-//   smooth: true,
-//   // // lerp: 0.4,
-//   multiplier: 2,
-//   touchMultiplier: 4,
-//   scrollFromAnywhere: true,
-//   // // repeat: true,
-//   // offset: ['30%', 0],
-//   // reloadOnContextChange: true,
-// });
+/* Rellax */
+const rellax = new Rellax('.rellax');
